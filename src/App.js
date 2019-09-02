e5a62e7c54967fc8cc0664fa11f96d0bd98b6db8
subin0025 <subin0025@naver.com>
2019-09-02 14:42:04 +0900

    서버 구현 및 REST API 연동

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import './App.css';
+import Coordinate from './components/Coordinate'
+
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
+
+
+
+class App extends Component {
+  state = {
+    coordinates: ''
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ coordinates: res }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/coordinate');
+    const body = await response.json();
+    return body;
+  }
+
+  render() {
+    return (
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>번호</TableCell>
+            <TableCell>이미지</TableCell>
+            <TableCell>세로위치</TableCell>
+            <TableCell>가로위치</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {this.state.coordinates ? this.state.coordinates.map(c => {
+            return <Coordinate key={c.id} id={c.id} name={c.name} row={c.row} col={c.col} />
+          }) : ''}
+        </TableBody>
+      </Table>
+    );
+  }
+
+}
+
+export default App;
